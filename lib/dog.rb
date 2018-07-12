8ff600a848eb58fc2847e031a92631b8546bf197
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-12 22:05:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,14 @@
 class Dog
 def bark
-puts "Woof"
+ puts "Woof"
 end
 
 def sit 
-  puts "
-
-
-
- puts "Woof"
- @sit = sit
- puts "The dog is stting"
- end 
+  puts " The dog is sitting"
 end
+
+
+
+ 
 
 
